c1ae3f4fe1e732edcfb881da1cf5ce0463a99956
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2013-12-15 21:31:55 -0500

    Bootstrap for basic layout

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
+	@links = [
+		{:name => 'LinkedIn', :href => 'http://www.linkedin.com'},
+		{:name => 'Twitter', :href => 'http://www.twitter.com'},
+		{:name => 'Github', :href => 'http://www.github.com'}
+	]
 	haml :index
+end
+
+not_found do
+	status 404
+	haml :'404'	
 end
