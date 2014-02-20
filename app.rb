cb9422c3d9b9bba9895dcb05987e9221eb8a1ae3
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2014-02-19 20:43:57 -0500

    Added LinkedIn connection and Bootstrap 3 Styles

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'linkedin'
 require 'haml'
+
+configure do
+	set :api, "75ck78s4p891h2"
+	set :secret, "nzFW2ktEp52yvyOD"
+	set :otoken, "e1701440-5e62-4609-818b-b46bc3ee4ab8"
+	set :osecret, "d2e0ca70-69e4-4172-992d-95cfcdf068d2"
+end
 
 get '/' do
 	@links = [
@@ -7,10 +15,12 @@ get '/' do
 		{:name => 'Twitter', :href => 'http://www.twitter.com'},
 		{:name => 'Github', :href => 'http://www.github.com'}
 	]
-	haml :index
+	client = LinkedIn::Client.new(settings.api, settings.secret)
+	client.authorize_from_access(settings.otoken, settings.osecret);
+	haml :index, :locals => { :profile => client.profile }
 end
 
 not_found do
 	status 404
-	haml :'404'	
+	haml :'404'
 end
