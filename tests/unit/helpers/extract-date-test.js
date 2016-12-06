96f98136c5f394ff063f3153b4e869eacf429107
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2016-12-06 01:06:40 -0500

    Added calendar icon in Blog Posts

---
+++ tests/unit/helpers/extract-date-test.js
@@ -0,0 +1,12 @@
+
+import { extractDate } from 'biannetta/helpers/extract-date';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | extract date');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = extractDate([42]);
+  assert.ok(result);
+});
+
