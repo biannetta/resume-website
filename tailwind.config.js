58e16619ea0b0b53602b1eece15413b54ee6babd
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2022-01-02 10:18:46 -0500

    Feature/tailwind (#6)
    * Basic Styling with Tailwind CSS
    * Changed Circle CI to only build on Master
    * Removed SCSS version as well

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./app/**/*.{html,hbs}'],
+  theme: {
+    fontFamily: {
+      openSans: ['"Open Sans"', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        blue: {
+          950: '#428bca',
+          1000: '#215c8e',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
