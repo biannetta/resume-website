db2959db84bcde42dd35f0c1a8715630f3432109
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2022-01-03 09:35:51 -0500

    Moved Profile of Skills into Firestore
    Pulling from database now instead of hardcoded

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,10 @@ import { inject as service } from '@ember/service';
 export default class IndexRoute extends Route {
   @service store;
 
-  model() {
-    return this.store.findAll('link');
+  async model() {
+    return {
+      links: this.store.findAll('link'),
+      skills: this.store.findAll('skill'),
+    };
   }
 }
