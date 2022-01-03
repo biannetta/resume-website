8a3ef493fb53e2e283d6dc193d6822f2c8f967f8
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2022-01-03 09:38:02 -0500

    Added 404 Page
    Also removed unused posts

--- app/models/post.js
+++
@@ -1,9 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class Post extends Model {
-  @attr('string') title;
-  @attr('string') body;
-  @attr('number') timestamp;
-  @attr('boolean') published;
-  @attr('string') slug;
-}
